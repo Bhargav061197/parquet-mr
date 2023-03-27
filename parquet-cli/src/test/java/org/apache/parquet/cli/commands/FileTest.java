501698a4bb10571e07a0a94bf7c7061b15d2442f
Bhargav <bhargavpaturu@gmail.com>
2023-03-27 21:33:11 +0530

    Replace log4j with reload4j dependency in parquet-cli

--- parquet-cli/src/test/java/org/apache/parquet/cli/commands/FileTest.java
+++ parquet-cli/src/test/java/org/apache/parquet/cli/commands/FileTest.java
@@ -24,6 +24,7 @@ import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.impl.Log4JLogger;
 
 import java.io.File;
 
